50f1e3d58eab3b7fcdf4f4f185724dcac98eccd5
himanbatra <95957864+himanbatra@users.noreply.github.com>
2024-12-06 19:33:32 +0000

    stringsdone

---
+++ 01_basics/04string.js
@@ -0,0 +1,21 @@
+// const name ="himanhsu "
+// const repocount=7
+
+// console.log(`hello my name is ${name} and my repocount is ${repocount}`)
+
+
+// const gamename= new String('gitesh_hc-com')
+// console.log(gamename[0])
+
+
+// cosnt string12="kuchbhi   "; 
+// console.log(string12)
+// console.log(string12.trim())
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+//console.log(url.includes('sundar'))
+
+
